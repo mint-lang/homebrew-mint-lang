b71eb361fcb31c27316a2d2e59b04c708f45e869
SeekingMeaning <meaningseeking@protonmail.com>
2020-02-25 10:41:22 -0800

    mint-lang: add linux binary

--- Formula/mint-lang.rb
+++ Formula/mint-lang.rb
@@ -1,14 +1,21 @@
+VERSION = "0.7.1".freeze
+
 class MintLang < Formula
   desc "Refreshing programming language for the front-end web"
   homepage "https://www.mint-lang.com/"
 
   stable do
-    url "https://github.com/mint-lang/mint/releases/download/0.7.1/mint-0.7.1-osx"
-    sha256 "5913f26524ffffeebdaa817c4e068e5dde3de6779e1631043aa1b18707bdf6a4"
+    if OS.mac?
+      url "https://github.com/mint-lang/mint/releases/download/#{VERSION}/mint-#{VERSION}-osx"
+      sha256 "5913f26524ffffeebdaa817c4e068e5dde3de6779e1631043aa1b18707bdf6a4"
+    elsif OS.linux?
+      url "https://github.com/mint-lang/mint/releases/download/#{VERSION}/mint-#{VERSION}-linux"
+      sha256 "7749ad65c5f201226f2aef2fe44f904b80102bdb7631b28a6cee1f2576c63ffd"
+    end
   end
 
   devel do
-    url "https://github.com/mint-lang/mint/archive/0.7.1.tar.gz"
+    url "https://github.com/mint-lang/mint/archive/#{VERSION}.tar.gz"
     sha256 "7081804496f3f29bb1b16cf9704d0a87218fb104961df8c208d804e099127460"
   end
 
@@ -20,7 +27,11 @@ class MintLang < Formula
 
   def install
     if build.stable?
-      bin.install buildpath/"mint-#{version}-osx" => "mint"
+      if OS.mac?
+        bin.install buildpath/"mint-#{VERSION}-osx" => "mint"
+      elsif OS.linux?
+        bin.install buildpath/"mint-#{VERSION}-linux" => "mint"
+      end
     else
       system "shards", "install"
       system "crystal", "build", "src/mint.cr", "-o", "mint", "-p", "--error-trace"
@@ -29,6 +40,6 @@ class MintLang < Formula
   end
 
   test do
-    assert_match "Mint #{version}", shell_output("#{bin}/mint version")
+    assert_match "Mint #{VERSION}", shell_output("#{bin}/mint VERSION")
   end
 end
